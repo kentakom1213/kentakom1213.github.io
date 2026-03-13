0d460f95e34d0bc2463628c984e1c027d2f21e1f
kentakom1213 <kentakom1213@gmail.com>
2026-03-13 11:47:30 +0900

    add: 他の部分の復元

---
+++ src/utils/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  title: "甲本健太 | KOMOTO Kenta",
+  description: "甲本 健太 (名古屋大学大学院 情報学研究科 数理情報学専攻)",
+  googleSiteVerification: "7Pja-gmgJ-5jKr55Kqomfcqz0kXFHnoUAHNyGARYCDo",
+  nameJa: "甲本 健太",
+  nameEn: "KOMOTO Kenta",
+  affiliation: "名古屋大学大学院 情報学研究科 数理情報学専攻",
+  email: "komoto.kenta.v1(at)s.mail.nagoya-u.ac.jp",
+} as const;
